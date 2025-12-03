558497212183c713940e50410f085b1491e79e7f
nothingmean00 <malik@novacai.com>
2025-12-03 18:16:04 -0500

    Update analytics import to next

--- .next/dev/static/chunks/Downloads_tech-sales-bible_app_not-found_tsx_bcf1717d._.js
+++ .next/dev/static/chunks/Downloads_tech-sales-bible_app_not-found_tsx_bcf1717d._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__e7570b41._.css",
-  "static/chunks/32e50_2452f5c5._.js"
+  "static/chunks/32e50_b8379f99._.js",
+  "static/chunks/Downloads_tech-sales-bible_8c7f109e._.js"
 ],
     source: "dynamic"
 });
